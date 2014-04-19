ff79781d27b63091e79f15a20b4cfb2192d67002
Pedro <pete@daffodil.uk.com>
2014-04-19 21:32:10 +0100

    request works

--- fggui/MainWindow.py
+++ fggui/MainWindow.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 
+import json
 
 from PyQt4 import QtCore, QtGui, QtNetwork
 from PyQt4.QtCore import Qt
+
+class CP:
+	node = 0
+	val = 1
+	type = 2
+	kids = 3
+	path = 4
 
 class MainWindow( QtGui.QMainWindow ):
 	
@@ -15,7 +23,7 @@ class MainWindow( QtGui.QMainWindow ):
 		
 		
 		self.netMan = QtNetwork.QNetworkAccessManager(self)
-		
+		self.connect(self.netMan , QtCore.SIGNAL("finished(QNetworkReply*)"), self.on_http_finished)
 		
 		## ========================================================
 		## Top Toolbar
@@ -31,39 +39,84 @@ class MainWindow( QtGui.QMainWindow ):
 		## Server Port
 		self.topToolBar.addWidget(QtGui.QLabel("fgms port:"))
 		self.txtPort = QtGui.QLineEdit()
-		self.txtPort.setText("9999")
+		self.txtPort.setText("8888")
 		self.topToolBar.addWidget(self.txtPort)
 		
 		self.buttRefresh = QtGui.QToolButton()
 		self.topToolBar.addWidget(self.buttRefresh)
 		self.buttRefresh.setText("Load Server")
 		self.connect( self.buttRefresh, QtCore.SIGNAL("clicked()"), self.send_request)
+
+
+		## ========================================================
+		## Tree
+		self.treeProps = QtGui.QTreeWidget()
+		self.setCentralWidget(self.treeProps)
+		
+		self.treeProps.setRootIsDecorated(True)
+		hi = self.treeProps.headerItem()
+		hi.setText(CP.node, "Path")
+		hi.setText(CP.val, "Value")
+		hi.setText(CP.type, "Type")
+		hi.setText(CP.kids, "Child Count")
+		
 		
 	def get_url(self):
 		
-		return self.txtHost.text().append(":").append( self.txtPort.text() ).append("/json/")
-
+		u = QtCore.QUrl()
+		u.setScheme("http")
+		u.setHost(self.txtHost.text())
+		port, ok =self.txtPort.text().toInt() 
+		u.setPort(port)
+		u.setPath("/json/")
+		return u
 	
 	def send_request(self):
 		
-		print self.get_url()
 		request = QtNetwork.QNetworkRequest()
-		request.setUrl( QtCore.QUrl( self.get_url()) )
+		request.setUrl( self.get_url() )
 		
-		self.reply = self.netMan.get( self.request )
+		self.reply = self.netMan.get( request )
+		
 		self.connect( self.reply, QtCore.SIGNAL( 'error(QNetworkReply::NetworkError)' ), self.on_http_error )
+		"""
 		self.connect( self.reply, QtCore.SIGNAL( 'readyRead()' ), self.on_http_read )
 		self.connect( self.reply, QtCore.SIGNAL( 'finished()' ), self.on_http_finished )
+		"""
 		
-		
-	def on_http_error(self):
-		pass
+	def on_http_error(self, err):
+		print "error", err
 	
 	def on_http_read(self):
 		print "read"
 		
-	def on_http_finished(self):
-		print "done"
+	def on_http_finished(self, reply):
+		
+		bytes = reply.readAll()
+		json_str = str(QtCore.QString.fromUtf8(bytes.data(), bytes.size()))
+		data = json.loads(json_str)
+		
+		self.load_data(data)
+		
+	def load_data(self, data):
+		
+		self.treeProps.setUpdatesEnabled(False)
+		for n in data:
+			print n
+			items = self.treeProps.findItems(n['path'], Qt.MatchExactly, CP.path)
+			if len(items) == 0:
+				# create a new node, and set everything here once, only value is updated later
+				item = QtGui.QTreeWidgetItem()
+				item.setText(CP.node, n['name'])
+				item.setText(CP.path, n['path'])
+				item.setText(CP.type, n['type'])
+			else:
+				# otherwise it exits fo first one
+				item = items[0]
+			#item.setText(CP.val, n[''])
+		self.treeProps.setUpdatesEnabled(True)
+		#print "done", reply
+		
 		
 		
 	
